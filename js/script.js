51d5782af8be1637fabd66716d84dd35ce23e3af
cqueiroga <chris.queiroga@gmail.com>
2023-12-12 12:13:29 -0500

    - Created operate function - Function takes in 3 args (2 numbers and 1 operator) - Based on operator will call function to calculate the 2 numbers

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
 // Declare operator variable
-let operator = null;
+const operator = "plus";
+
+// Declare variable for number input and total
+let num1 = 1;
+let num2 = 1;
+let total;
 
 // Declare calculation functions
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+// Declare operate function
+// Accepts three arguments - 2 numbers and operator
+// Calls calculation function based on operator
+function operate(a, b, operator) {
+	if (operator == "plus") {
+		return add(a, b);
+	} else if (operator == "minus") {
+		return subtract(a, b);
+	} else if (operator == "multiply") {
+		return multiply(a, b);
+	} else if (operator == "divide") {
+		return divide(a, b);
+	}
+}
+
+console.log(operate(num1, num2, operator));
