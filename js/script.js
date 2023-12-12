4aba070299061391d8d9308e8b6adbf7bb8f6534
cqueiroga <chris.queiroga@gmail.com>
2023-12-12 09:56:17 -0500

    - Created project file/folder structure - Created calculation functions for add, subtract, multiply and divide

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Declare operator variable
+let operator = null;
+
+// Declare calculation functions
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
