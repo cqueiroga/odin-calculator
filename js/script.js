7ddf9ccfccfebc969f2a056c683d3a00ac12e097
cqueiroga <chris.queiroga@gmail.com>
2023-12-14 19:06:12 -0500

    - Added event listener for positive/negative button - Updates values to be positive or negative before being calculated - Created event listener for decimal button - If decimal is already present, disable decimal button to prevent adding two decimals - Added rounding function to round total if decimals are needed - Added event listener for percentage button

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ let num2 = 0;
 
 let formula = document.getElementById("formula");
 let formulaText = formula.innerHTML;
+document.getElementById("output").maxLength = 5;
 let output = document.getElementById("output");
 
 const errorMsg = "Cannot divide by zero";
@@ -25,12 +26,16 @@ const divide = (a, b) => {
 	}
 };
 
+// Button elements
 const btn = document.querySelectorAll("button");
 const numeric = document.getElementsByClassName("numeric");
 const operatorBtn = document.getElementsByClassName("operator");
 const clearBtn = document.getElementById("clear");
 const clearEntry = document.getElementById("clear-entry");
 const equalsBtn = document.getElementById("equals");
+const posandnegBtn = document.getElementById("pos-neg-btn");
+const decimalBtn = document.getElementById("decimal-btn");
+const percentageBtn = document.getElementById("percentage");
 
 // Event listener for numeric buttons
 for (i of numeric) {
@@ -46,6 +51,7 @@ for (i of numeric) {
 				output.innerHTML = num2;
 			} else {
 				num2 += this.value;
+				checkLength();
 				output.innerHTML = num2;
 			}
 		} else if (num1 == 0) {
@@ -53,9 +59,21 @@ for (i of numeric) {
 			output.innerHTML = num1;
 		} else {
 			num1 += this.value;
+			checkLength();
 			output.innerHTML = num1;
 		}
 	});
+}
+
+// Limit the amount of numbers allowed
+// Disable numeric buttons if greater than 16 numbers
+function checkLength() {
+	let tempString = output.innerHTML.split("");
+	if (tempString.length > 14) {
+		for (i of numeric) {
+			i.disabled = true;
+		}
+	}
 }
 
 // Event listener for operator buttons
@@ -76,6 +94,9 @@ for (i of operatorBtn) {
 			operator == "×" ||
 			operator == "÷"
 		) {
+			for (i of numeric) {
+				i.removeAttribute("disabled");
+			}
 			formula.innerHTML = num1 + " " + operator + " ";
 			output.innerHTML = num1;
 		}
@@ -126,13 +147,23 @@ function equals(btn) {
 	if (num1 == 0 && num2 == 0) {
 		output.innerHTML = 0;
 	} else {
-		num1 = parseInt(num1);
-		num2 = parseInt(num2);
+		num1 = Number(num1);
+		num2 = Number(num2);
 		total = operate(num1, num2, operator);
+		roundTotal(total);
 		num1 = total;
 		formula.innerHTML += " " + num2 + " " + btn.value;
-		output.innerHTML = total;
+		output.innerHTML = num1;
 		num2 = 0;
+	}
+}
+
+// Function to round total to two decimal places
+function roundTotal(total) {
+	if (Number.isInteger(total)) {
+		return total;
+	} else {
+		return total.toFixed(2);
 	}
 }
 
@@ -145,3 +176,43 @@ function clear() {
 	num1 = 0;
 	num2 = 0;
 }
+
+// Event listener to change numbers to negative numbers
+posandnegBtn.addEventListener("click", function () {
+	let num = 0;
+	if (output.innerHTML != 0) {
+		if (!operator) {
+			num = output.innerHTML * -1;
+			num1 = num;
+			output.innerHTML = num1;
+		} else {
+			num = output.innerHTML * -1;
+			num2 = num;
+			output.innerHTML = num2;
+		}
+	}
+});
+
+// Event listener for decimal
+// if decimal is already present disable decimal button
+decimalBtn.addEventListener("click", function () {
+	if (!output.innerHTML.includes(".")) {
+		if (!operator) {
+			num1 += this.value;
+			output.innerHTML = num1;
+		} else {
+			num2 += this.value;
+			output.innerHTML = num2;
+		}
+	} else {
+		decimalBtn.disabled = true;
+	}
+});
+
+// Event listener to calculate percentage
+percentageBtn.addEventListener("click", function () {
+	if (operator) {
+		num2 = (num1 / 100) * num2;
+		output.innerHTML = num2;
+	}
+});
